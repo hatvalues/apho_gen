c0136d54d2a2757bf9a4e734a4b2e6faf9931c16
hatvalues <125478155+hatvalues@users.noreply.github.com>
2024-02-01 09:03:37 +0000

    fix tests

--- tests/test_app_logic.py
+++ tests/test_app_logic.py
@@ -1,6 +1,7 @@
 from app_logic.inflect_words import get_plurals
 
-plurals = get_plurals("this is the house, this is the hill")
-assert plurals.pop("this") == "these"
-for k, v in plurals.items():
-    assert v == f"{k}s"
+def test_get_plurals():
+    plurals = get_plurals("this is the house, this is the hill")
+    assert plurals.pop("this") == "these"
+    for k, v in plurals.items():
+        assert v == f"{k}s"
