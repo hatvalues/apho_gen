f549a37381ae32d08d064a6f0b20a1cc44ce40c7
hatvalues <julian@hatvalues.org>
2024-02-01 13:43:10 +0000

    added app and docker file

--- app_logic/inflect_words.py
+++ app_logic/inflect_words.py
@@ -5,11 +5,13 @@ from collections import defaultdict
 def pre_process(text):
     return TextBlob(text)
 
+
 def inflect_plur(blob):
     plural_words = defaultdict(str)
     for word in blob.words:
         plural_words[word] = word.pluralize()
     return dict(plural_words)
+
 
 def get_plurals(text):
     blob = pre_process(text)
---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import uvicorn
+from app_logic import inflect_words
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    """Health Check"""
+    return "OK"
+
+
+@app.get("/pluralize/{text}")
+async def pluralize(text: str):
+    """Gets a dict of plural words from text.
+    Some are not valid"""
+    return inflect_words.get_plurals(text)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
